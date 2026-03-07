7ca0fb16ba8527ddd70ae5b9f18ae5c4e7167cae
Nicolas Cartier <ecorcheur.fr@gmail.com>
2026-03-08 09:38:05 +1100

    testfinale

---
+++ execiceIII-3-TP05 .py
@@ -0,0 +1,4 @@
+a = (3)
+b = (2)
+somme = a + b
+print(somme)
--- execiceIII-4-TP05 .py
+++ execiceIII-4-TP05 .py
@@ -1,2 +1,10 @@
-print("bonjour") 
-print("test")
+def trier_sommes(liste):
+    return sorted(liste)
+a = int(input("Entrez a : "))
+b = int(input("Entrez b : "))
+c = int(input("Entrez c : "))
+d = int(input("Entrez d : "))
+e = int(input("Entrez e : "))
+sommes = [a, b, c, d, e]
+sommesT = trier_sommes(sommes)
+print(sommesT)
